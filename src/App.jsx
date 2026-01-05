d530d26c742f872102f593803781451b9d20c6ea
ian-michael254 <ian.michael.codes@gmail.com>
2026-01-05 22:18:55 +0300

    project setup

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import 'bootstrap-icons/font/bootstrap-icons.css';
+
+
+
+const App = () => {
+  return (
+    <div className='text-4xl text-center bg-violet-200'>
+      <i className="bi bi-battery-full text-red-500"></i>
+    </div>
+  )
+}
+
+export default App
