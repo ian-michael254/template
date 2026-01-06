c01b98fafb13b2a75294ac41bf53a9f6ec6e107e
ian-michael254 <ian.michael.codes@gmail.com>
2026-01-07 00:31:53 +0300

    about  component

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -5,7 +5,7 @@ const About = () => {
   return (
     <div className='flex flex-col items-center justify-center container mx-auto p-14 md:px-20 lg:px-32 w-full overflow-hidden' id="About">
         <h1 className='text-2xl sm:text-4xl font-bold mb-2'>About <span className='underline underline-offset-4 decoration-1 under font-light'>Our Brand</span></h1>
-        <p className='text-blue-200 max-w-80 text-center mb-8'>Passionate About Properties, Dedicated to your Vision</p>
+        <p className='text-gray-500 max-w-80 text-center mb-8'>Passionate About Properties, Dedicated to your Vision</p>
         <div className='flex flex-col md:flex-row items-center md:items-start md:gap-20'>
             <video src={assets.brand_video} className='w-full  sm:w-1/2 max-w-lg'autoPlay muted loop />
             
