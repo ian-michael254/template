a2063d00501427624935849eba950a6d63b21bae
ian-michael254 <ian.michael.codes@gmail.com>
2026-01-07 00:50:46 +0300

    final product

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -5,14 +5,14 @@ const About = () => {
   return (
     <div className='flex flex-col items-center justify-center container mx-auto p-14 md:px-20 lg:px-32 w-full overflow-hidden' id="About">
         <h1 className='text-2xl sm:text-4xl font-bold mb-2'>About <span className='underline underline-offset-4 decoration-1 under font-light'>Our Brand</span></h1>
-        <p className='text-gray-500 max-w-80 text-center mb-8'>Passionate About Properties, Dedicated to your Vision</p>
+        <p className='text-gray-500 max-w-80 text-center mb-8'>Passionate About cars, Dedicated to your Vision</p>
         <div className='flex flex-col md:flex-row items-center md:items-start md:gap-20'>
             <video src={assets.brand_video} className='w-full  sm:w-1/2 max-w-lg'autoPlay muted loop />
             
             <div className='flex flex-col items-center md:items-start mt-10 text-slate-800'>
                 <div className="grid grid-cols-2 gap-6 md:ap-10 w-full 2xl:pr-28">
                     <div>
-                        <p className='text-4xl font-medium text-gray-800'>10+</p>
+                        <p className='text-4xl font-medium text-gray-800'>5+</p>
                         <p>Years of Excellence</p>
                     </div>
 
@@ -25,7 +25,7 @@ const About = () => {
                         <p>Min Project Time Taken</p>
                     </div>
                     <div>
-                        <p className='text-4xl font-medium text-gray-800'>50+</p>
+                        <p className='text-4xl font-medium text-gray-800'>10+</p>
                         <p>Ongoing Projects</p>
                     </div>
                 </div>
